6337d5c3568b414272efb570b765a4f83de8742f
Adrian D. Alvarez <adrian.alvarez@touchstone-group.com>
2015-09-18 09:55:03 -0400

    Initial commit

---
+++ src/Saika/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Saika.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
