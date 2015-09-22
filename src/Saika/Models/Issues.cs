f60037ba47fbcaeb01d1f6d97e6c272530fd0112
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-09-21 21:51:41 -0400

    Changed Vulnerabilities to Issues

---
+++ src/Saika/Models/Issues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Saika.Models
+{
+    public class Issues
+    {
+        public Guid IssueId { get; set; }
+        public string IssueTitle { get; set; }
+        public string AttackSteps { get; set; }
+        public string Notes { get; set; }
+        public string AffectedPaths { get; set; }
+    }
+}
