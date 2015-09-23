1fb68bab723f7dde8c7f0b3ce866bd8cd2d0ae9f
Adrian D. Alvarez <adrian.alvarez@touchstone-group.com>
2015-09-23 14:02:06 -0400

    Experimentation with sample for vulnerabilities based off of vulndb

--- src/Saika/Models/Issues.cs
+++ src/Saika/Models/Issues.cs
@@ -9,8 +9,11 @@ namespace Saika.Models
     {
         public Guid IssueId { get; set; }
         public string IssueTitle { get; set; }
+        public string VulnerabilityTestType { get; set; }  //Manual - Automated Scan - Online Scan/Service
+        public string VulnerabilityTestTool { get; set; } //Name of tool
+        public string VulnerabilityTestToolUrl { get; set; } //Url of tools / sites used.
         public string AttackSteps { get; set; }
-        public string Notes { get; set; }
         public string AffectedPaths { get; set; }
+        public string Notes { get; set; }
     }
 }
